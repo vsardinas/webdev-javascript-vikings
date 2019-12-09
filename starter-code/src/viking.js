5d22243362d04984213ba73caaedd59a31dad43a
Vincent Sardinas <22344@columbushs.com>
2019-12-09 08:43:15 -0500

    commit

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,30 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+    this.health = health;
+    this.strength =strength;
+
+}
+
+attack() {
+return this.strength
+}
+
+receiveDamage(theDamage) {
+    this.health-= theDamage
+
+}
+}
+
+
+
+
 
 // Viking
-class Viking {}
+class Viking {
+    class Viking extends Soldier {
+    constructor (name, )}
+}
 
 // Saxon
 class Saxon {}
